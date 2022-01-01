d3922cfe90c0fb0c030afc387d00321b7e3bca9b
jy_pro <jyEmail_pro@qq.com>
2022-01-01 08:06:06 +0800

    add cross to controller

--- src/main/java/com/bilitech/yilimusic/controller/DefaultController.java
+++ src/main/java/com/bilitech/yilimusic/controller/DefaultController.java
@@ -1,5 +1,6 @@
 package com.bilitech.yilimusic.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/hello")
+@CrossOrigin
 public class DefaultController {
 
     @RequestMapping
